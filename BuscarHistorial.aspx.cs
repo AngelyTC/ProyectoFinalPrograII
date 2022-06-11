c4db1f0f9224efd5d278c7332bf6b7e8cdccce76
Allgm_03 <Allgm_03@mesoamericana.edu.gt>
2022-06-11 15:58:58 -0600

    Allgm_03@mesoamericana.edu.gt

---
+++ BuscarHistorial.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ProyectoFinalPrograII
+{
+    public partial class BuscarHistorial : System.Web.UI.Page
+    {
+        List<HistorialPacienteC> paciente = new List<HistorialPacienteC>();
+        List<HistorialPacienteC> pacienteTemp = new List<HistorialPacienteC>();
+        string codigo;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string archivo = Server.MapPath("Consultas.json");
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();
+            jsonStream.Close();
+            pacienteTemp = JsonConvert.DeserializeObject<List<HistorialPacienteC>>(json);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            codigo = TextBox1.Text;
+            bool encontrar = false;
+
+            foreach (var u in pacienteTemp)
+            {
+
+                HistorialPacienteC buscarp = pacienteTemp.Find(a => a.NITPaciente == codigo);
+
+                if (buscarp != null)
+                {
+                    paciente.Add(buscarp);
+                    GridView1.DataSource = paciente;
+                    GridView1.DataBind();
+                    encontrar = true;
+
+                }
+            }
+
+            if (!encontrar)
+            {
+                Response.Write("<script>alert('El paciente no tiene historial.')</script>");
+
+
+                codigo = " ";
+                TextBox1.Text = " ";
+
+
+            }
+        }
+    }
+}
