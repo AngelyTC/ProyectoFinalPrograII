da263c83ddabcaaeca19d64beb4a4b9963d9115f
AngelyTC <angelyth312@mesoamericana.edu.gt>
2022-06-10 18:01:36 -0600

    IngresoSíntomas -  AngelyThomas angelyth321@mesoamericana.edu.gt

---
+++ IngresoSintomas.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="IngresoSintomas.aspx.cs" Inherits="ProyectoFinalPrograII.IngresoSintomas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p style="font-size: medium">
+        <asp:Label ID="Label1" runat="server" ForeColor="Blue" style="font-size: x-large" Text="SÍNTOMAS"></asp:Label>
+        <br />
+    </p>
+    <p style="font-size: medium">
+        Código Síntoma</p>
+    <p>
+        <asp:TextBox ID="txtCodigoS" runat="server" style="font-size: medium"></asp:TextBox>
+    </p>
+    <p style="font-size: medium">
+        Descripción Síntoma</p>
+    <p>
+        <asp:TextBox ID="txtDescripcionS" runat="server" style="font-size: medium"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="btnIngresoS" runat="server" OnClick="btnIngresoS_Click" Text="Ingreso Síntoma" style="font-size: medium" />
+    </p>
+    <p>
+        <asp:Button ID="btnLimpiar" runat="server" OnClick="btnLimpiar_Click" style="font-size: large" Text="Limpiar" />
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+</asp:Content>
---
+++ IngresoSintomas.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ProyectoFinalPrograII
+{
+
+
+    public partial class IngresoSintomas
+    {
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control txtCodigoS.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodigoS;
+
+        /// <summary>
+        /// Control txtDescripcionS.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcionS;
+
+        /// <summary>
+        /// Control btnIngresoS.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIngresoS;
+
+        /// <summary>
+        /// Control btnLimpiar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLimpiar;
+    }
+}
