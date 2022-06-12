8efde8547db5767bd4c7c0ba71b19edb2e09b539
AngelyTC <angelyth312@mesoamericana.edu.gt>
2022-06-11 21:49:16 -0600

    Allgm_03@mesoamericana.edu.gt

---
+++ ReporteDinero.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ReporteDinero.aspx.cs" Inherits="ProyectoFinalPrograII.ReporteDinero" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        REPORTE DINERO:<br />
+    </p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server" Height="269px" Width="529px">
+        </asp:GridView>
+    </p>
+    <p>
+        <asp:Label ID="Label1" runat="server" Text="Total:"></asp:Label>
+        <asp:Label ID="Label2" runat="server" Text="-"></asp:Label>
+    </p>
+    <p>
+        <asp:Button ID="btnVisualizar" runat="server" OnClick="btnVisualizar_Click" Text="Visualizar" />
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+</asp:Content>
---
+++ ReporteDinero.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ProyectoFinalPrograII
+{
+    public partial class ReporteDinero : System.Web.UI.Page
+    {
+        List<HistorialPacienteC> Temp = new List<HistorialPacienteC>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String archivo = Server.MapPath("Consultas.json");
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();
+            jsonStream.Close();
+            Temp = JsonConvert.DeserializeObject<List<HistorialPacienteC>>(json);
+        }
+
+        protected void btnVisualizar_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+
+            foreach (var u in Temp)
+            {
+
+                num1 += Convert.ToInt32(u.CostoConsultaP);
+                Label2.Text = num1.ToString();
+                GridView1.DataSource = Temp;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
