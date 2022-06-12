3b1e330b3f76848235cc1ec92c2b9929dc12ae60
AngelyTC <angelyth312@mesoamericana.edu.gt>
2022-06-11 23:44:18 -0600

    angelyth321@mesoamericana.edu.gt

---
+++ Login.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace ProyectoFinalPrograII
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+            
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string usuario = Login1.UserName;
+            string password = Login1.Password;
+
+            if ((usuario == "Doctor") && (password == "123"))
+            {
+                FormsAuthenticationTicket tkt;
+                string cookiestr;
+                HttpCookie ck;
+                tkt = new FormsAuthenticationTicket(1, Login1.UserName, DateTime.Now,
+                DateTime.Now.AddMinutes(30), Login1.RememberMeSet, "algun dato extra");
+                cookiestr = FormsAuthentication.Encrypt(tkt);
+                ck = new HttpCookie(FormsAuthentication.FormsCookieName, cookiestr);
+                if (Login1.RememberMeSet)
+                    ck.Expires = tkt.Expiration;
+                ck.Path = FormsAuthentication.FormsCookiePath;
+                Response.Cookies.Add(ck);
+
+                string strRedirect;
+                strRedirect = Request["ReturnUrl"];
+                if (strRedirect == null)
+                    strRedirect = "Default.aspx";
+
+                Response.Redirect(strRedirect, true);
+            }
+          
+            }
+        }
+    }
--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -11,16 +11,16 @@ namespace ProyectoFinalPrograII
 {
 
 
-    public partial class SiteMaster
+    public partial class Login
     {
 
         /// <summary>
-        /// Control MainContent.
+        /// Control Login1.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.Login Login1;
     }
 }
