7cd88617e176d9f34aab31857f2b33f3f9ed74df
Allgm_03 <Allgm_03@mesoamericana.edu.gt>
2022-06-11 18:15:42 -0600

    Allgm_03@mesoamericana.edu.gt

---
+++ ReporteMedicamentos.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ReporteMedicamentos.aspx.cs" Inherits="ProyectoFinalPrograII.ReporteMedicamentos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <br />
+        Medicamentos más utilizados:</p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </p>
+    <p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Visualizar" />
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+</asp:Content>
---
+++ ReporteMedicamentos.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ProyectoFinalPrograII
+{
+    public partial class ReporteMedicamentos : System.Web.UI.Page
+    {
+        List<MedicamentosC> mediTemp = new List<MedicamentosC>();
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string archivo = Server.MapPath("Medi.json");
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();
+            jsonStream.Close();
+            mediTemp = JsonConvert.DeserializeObject<List<MedicamentosC>>(json);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+      
+            foreach (var a in mediTemp)
+            {
+                MedicamentosC buscar = mediTemp.Find(c => c.CodigoMedicamento != null);
+                               
+
+                if (a.CodigoMedicamento != null)
+                {
+                    GridView1.DataSource = mediTemp;
+                    GridView1.DataBind();
+                }
+            }
+
+        }
+    }
+}
